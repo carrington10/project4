9d7c7e28335668afdedeaa4b712eb2f59032e778
Reagan Wood <reaganwood1@gmail.com>
2018-04-27 11:09:16 -0500

    Function wasn't being defined before it was being used, switched around the order

--- longsubstr.c
+++ longsubstr.c
@@ -13,6 +13,25 @@
 
 #define THREADS_COUNT 2
 
+unsigned find_longest_substr(char *str1, char *str2, char ** write_to) {
+    unsigned cnt;
+    unsigned longest_length = 0;
+    char *ptr1 = str1;
+    
+    for(; *str2; str1 = ptr1, str2++ ) {
+        for(; *str1; str1++ )
+        {
+            cnt = match_count(str1,str2);
+            if(longest_length < cnt ) {
+                longest_length = cnt;
+                *write_to = str1;
+            }
+        }
+    }
+    
+    return longest_length;
+}
+
 typedef struct {
 	int rank;
 	char ** line_ptrs;	
@@ -41,26 +60,6 @@ unsigned match_count (char *str1, char *str2) {
     }
         
     return i;
-}
-
-
-unsigned find_longest_substr(char *str1, char *str2, char ** write_to) {
-    unsigned cnt;
-    unsigned longest_length = 0;
-    char *ptr1 = str1;
-    
-    for(; *str2; str1 = ptr1, str2++ ) {
-        for(; *str1; str1++ )
-        {
-            cnt = match_count(str1,str2);
-            if(longest_length < cnt ) {
-                longest_length = cnt;
-                *write_to = str1;
-            }
-        }
-    }
-    
-    return longest_length;
 }
 
 
